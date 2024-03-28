a9ba7f5e9a182fcb5f48d97537b9a67228364f15
Vaughan Kitchen <contact@tophat.nz>
2024-03-28 20:10:57 +1300

    Ruby indexer

---
+++ JASSjr_index.rb
@@ -0,0 +1,91 @@
+#!/usr/bin/env ruby
+
+# Copyright (c) 2024 Vaughan Kitchen
+# Minimalistic BM25 search engine.
+
+# Make sure we have one parameter, the filename
+abort("Usage: #{$0} <infile.xml>") if ARGV.length != 1
+
+vocab = {} # the in-memory index
+doc_ids = [] # the primary keys
+length_vector = [] # hold the length of each document
+
+docid = -1
+document_length = 0
+push_next = false # is the next token the primary key?
+
+File.foreach(ARGV[0]) do |line|
+  # A token is either an XML tag '<'..'>' or a sequence of alpha-numerics.
+  # TREC <DOCNO> primary keys have a hyphen in them
+  line.scan(/[a-zA-Z0-9][a-zA-Z0-9-]*|<[^>]*>/).each do |token|
+    # If we see a <DOC> tag then we're at the start of the next document
+    if token == "<DOC>"
+      # Save the previous document length
+      length_vector << document_length if docid != -1
+      # Move on to the next document
+      docid += 1
+      document_length = 0
+      puts("#{docid} documents indexed") if docid % 1000 == 0
+    end
+    # if the last token we saw was a <DOCNO> then the next token is the primary key
+    if push_next
+      doc_ids << token.dup # Duplicate so that the primary key doesn't get downcased
+      push_next = false
+    end
+    push_next = true if token == "<DOCNO>"
+    # Don't index XML tags
+    next if token[0] == "<"
+
+    # lower case the string
+    token.downcase!
+
+    # truncate any long tokens at 255 charactes (so that the length can be stored first and in a single byte)
+    token = token.slice(0, 255)
+
+    # add the posting to the in-memory index
+    vocab[token] = [] if !vocab.key?(token)
+    postings_list = vocab[token]
+    if postings_list.length == 0 || postings_list[-2] != docid
+      postings_list << docid << 1
+    else
+      postings_list[-1] += 1
+    end
+
+    # Compute the document length
+    document_length += 1
+  end
+end
+
+# If we didn't index any documents then we're done.
+exit if docid == -1
+
+# tell the user we've got to the end of parsing
+puts("Indexed #{docid + 1} documents. Serialising...")
+
+# Save the final document length
+length_vector << document_length
+
+# store the primary keys
+File.open("docids.bin", "w") { |file| file.puts(doc_ids) }
+
+postings_fp = File.open("postings.bin", "wb")
+vocab_fp = File.open("vocab.bin", "wb")
+
+vocab.each do |term, postings|
+  # write the postings list to one file
+  where = postings_fp.pos
+  postings_fp.write(postings.pack("l*"))
+
+  # write the vocabulary to a second file (one byte length, string, '\0', 4 byte where, 4 byte size)
+  vocab_fp.write([term.length].pack("C"))
+  vocab_fp.write(term)
+  vocab_fp.write("\0")
+  vocab_fp.write([where, postings.length * 4].pack("ll"))
+end
+
+# store the document lengths
+File.open("lengths.bin", "w") { |file| file.write(length_vector.pack("l*")) }
+
+# clean up
+postings_fp.close
+vocab_fp.close
