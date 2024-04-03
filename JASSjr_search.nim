9041b8989ba48e066913ef77c16ff87e5733aff3
Vaughan Kitchen <contact@tophat.nz>
2024-04-04 10:32:59 +1300

    BM25

--- JASSjr_search.nim
+++ JASSjr_search.nim
@@ -3,6 +3,9 @@
 # Copyright (c) 2024 Vaughan Kitchen
 # Minimalistic BM25 search engine.
 
+import std/algorithm
+import std/math
+import std/sequtils
 import std/streams
 import std/strformat
 import std/strutils
@@ -22,6 +25,8 @@ while not doc_lengths_strm.atEnd():
 
 doc_lengths_strm.close()
 
+let average_length = doc_lengths.foldl(a + b) / len(doc_lengths)
+
 var postings: seq[tuple[docid: int32, tf: int32]]
 
 var vocab = initTable[string, tuple[where: int32, length: int32]]()
@@ -37,11 +42,20 @@ while not vocab_strm.atEnd():
 
 vocab_strm.close()
 
+var accumulators = newSeq[(float, int)](len(doc_ids))
+for i, _ in accumulators:
+  accumulators[i][1] = i
+
 try:
   while true:
     let query = readLine(stdin)
 
     let terms = query.splitWhitespace()
+    if len(terms) == 0:
+      continue
+
+    for i, _ in accumulators:
+      accumulators[i][0] = 0
 
     for term in terms:
       try:
@@ -51,6 +65,7 @@ try:
 
         let postings_fh = open("postings.bin")
         postings_fh.setFilePos(where)
+
         let postings_strm = newFileStream(postings_fh)
 
         for i in 0 .. length:
@@ -59,11 +74,20 @@ try:
           postings.add((docid, tf))
 
         postings_strm.close() # strm owns fh
-        for i, (docid, tf) in postings:
-          if i == 10:
-            break
-          echo(fmt"0 Q0 {doc_ids[docid]} {i+1} {tf} JASSjr")
+
+        let idf = ln(len(doc_lengths) / len(postings))
+
+        for (docid, tf) in postings:
+          let rsv = idf * float(tf) * (k1 + 1) / (float(tf) + k1 * (1 - b + b * (float(doc_lengths[docid]) / average_length)))
+          accumulators[docid][0] += rsv
       except KeyError:
         discard
+
+    accumulators.sort(Descending)
+
+    for i, (rsv, docid) in accumulators:
+      if rsv == 0 or i == 10:
+        break
+      echo(fmt"0 Q0 {doc_ids[docid]} {i+1} {rsv} JASSjr")
 except EOFError:
   discard
