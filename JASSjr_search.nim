4414facdff44dc5ddad89966467f756d3d7d60dd
Vaughan Kitchen <contact@tophat.nz>
2024-04-04 08:07:46 +1300

    Only read relevant postings

--- JASSjr_search.nim
+++ JASSjr_search.nim
@@ -23,28 +23,34 @@ while not doc_lengths_strm.atEnd:
 doc_lengths_strm.close()
 
 var postings: seq[tuple[docid: int32, tf: int32]]
-let postings_strm = newFileStream("postings.bin", fmRead)
-
-while not postings_strm.atEnd:
-  let docid = postings_strm.readInt32
-  let tf = postings_strm.readInt32
-  postings.add((docid, tf))
-
-postings_strm.close()
 
 var vocab = initTable[string, tuple[where: int32, length: int32]]()
 let vocab_strm = newFileStream("vocab.bin", fmRead)
+
 while not vocab_strm.atEnd:
   let term_length = vocab_strm.readUint8
   let term = vocab_strm.readStr(int(term_length))
   discard vocab_strm.readUint8
   let where = vocab_strm.readInt32
   let length = vocab_strm.readInt32
-  vocab[term] = (where div 8, length div 8)
+  vocab[term] = (where, length div 8)
 
 vocab_strm.close()
 
 let query = readLine(stdin).strip
 let (where, length) = vocab[query]
-for i, (docid, tf) in postings[where..where+length-1]:
-  echo(fmt"0 Q0 {doc_ids[docid]} {i+1} {tf} JASSjr")
+
+postings.setLen(0)
+
+let postings_fh = open("postings.bin")
+postings_fh.setFilePos(where)
+let postings_strm = newFileStream(postings_fh)
+
+for i in 0 .. length:
+  let docid = postings_strm.readInt32
+  let tf = postings_strm.readInt32
+  postings.add((docid, tf))
+
+postings_strm.close() # strm owns fh
+for i, (docid, tf) in postings:
+   echo(fmt"0 Q0 {doc_ids[docid]} {i+1} {tf} JASSjr")
