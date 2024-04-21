7b74d06dbf3def7102c6f9590e2034202d80096f
Vaughan Kitchen <contact@tophat.nz>
2024-04-21 16:58:43 +1200

    QA helpers

---
+++ tools/verify_indexer.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024 Vaughan Kitchen
+
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <prog> <infile>"
+	exit
+fi
+
+if [ ! -f stdout.gold.bin -o ! -f results.gold.bin -o ! -f docids.gold.bin -o ! -f lengths.gold.bin -o ! -f postings.gold.bin -o ! -f vocab.gold.bin ]; then
+	echo "Generating gold standard with JASSjr_index"
+	./JASSjr_index "$2" > stdout.gold.bin
+	./JASSjr_search < 51-100.titles.txt > results.gold.bin
+	mv docids.bin docids.gold.bin
+	mv lengths.bin lengths.gold.bin
+	mv postings.bin postings.gold.bin
+	mv vocab.bin vocab.gold.bin
+fi
+
+echo "Verifying $1"
+./"$@" > stdout.bin
+
+correct=true
+
+if ! cmp stdout.bin stdout.gold.bin; then
+	echo "ERROR: stdout differs"
+	correct=false
+fi
+
+if ! cmp docids.bin docids.gold.bin; then
+	echo "ERROR: docids.bin differs"
+	correct=false
+fi
+
+if ! cmp lengths.bin lengths.gold.bin; then
+	echo "ERROR: lengths.bin differs"
+	correct=false
+fi
+
+if [ "$(wc -c postings.bin | cut -d ' ' -f 1)" != "$(wc -c postings.gold.bin | cut -d ' ' -f 1)" ]; then
+	echo "ERROR: postings.bin differs in length"
+	correct=false
+fi
+
+if [ "$(wc -c vocab.bin | cut -d ' ' -f 1)" != "$(wc -c vocab.gold.bin | cut -d ' ' -f 1)" ]; then
+	echo "ERROR: vocab.bin differs in length"
+	correct=false
+fi
+
+echo "Verifying example queries"
+./JASSjr_search < 51-100.titles.txt > results.bin
+if ! cmp results.bin results.gold.bin; then
+	echo "ERROR: example queries results differs"
+	correct=false
+fi
+
+if $correct; then
+	echo "$1 appears to be correct"
+fi
---
+++ tools/verify_search.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024 Vaughan Kitchen
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <prog>"
+	exit
+fi
+
+if [ -f results.gold.bin -a -f docids.gold.bin -a -f lengths.gold.bin -a -f postings.gold.bin -a -f vocab.gold.bin ]; then
+	cp docids.gold.bin docids.bin
+	cp lengths.gold.bin lengths.bin
+	cp postings.gold.bin postings.bin
+	cp vocab.gold.bin vocab.bin
+else
+	echo "Generating gold standard with JASSjr_index"
+	./JASSjr_index "$2" > stdout.gold.bin
+	./JASSjr_search < 51-100.titles.txt > results.gold.bin
+	mv docids.bin docids.gold.bin
+	mv lengths.bin lengths.gold.bin
+	mv postings.bin postings.gold.bin
+	mv vocab.bin vocab.gold.bin
+fi
+
+echo "Verifying $1"
+./"$@" < 51-100.titles.txt > results.bin
+
+if ! cmp results.bin results.gold.bin; then
+	echo "ERROR: example queries results differs"
+else
+	echo "$1 appears to be correct"
+fi
